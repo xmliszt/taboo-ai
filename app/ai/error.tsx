d6920cba3b21066e45e9547b6322dbac0a541acd
Li Yuxuan <xmliszt@gmail.com>
2023-01-16 16:35:01 +0800

    Feature: AI mode page add dark theme

--- app/ai/error.tsx
+++ app/ai/error.tsx
@@ -14,11 +14,13 @@ export default function Error({
   }, [error]);
 
   return (
-    <div className="w-full h-screen flex flex-col justify-center items-center gap-10">
-      <h1 className="text-3xl lg:text-5xl text-white">Something went wrong!</h1>
+    <div className="w-full h-screen flex flex-col justify-center items-center gap-10 dark:bg-neon-black">
+      <h1 className="text-3xl lg:text-5xl text-white dark:text-neon-white">
+        Something went wrong!
+      </h1>
       <button
         id="reset"
-        className="text-2xl lg:text-4xl text-red hover:text-white transition-all"
+        className="text-2xl lg:text-4xl text-red dark:text-neon-red hover:dark:text-neon-white hover:text-white transition-all"
         onClick={() => reset()}
       >
         Reset
--- app/ai/page.tsx
+++ app/ai/page.tsx
@@ -44,14 +44,14 @@ export default function AiPage() {
       />
       <section
         className={`w-full h-screen flex justify-center items-center transition-colors ease-in-out ${
-          isValid || somethingWrong ? "" : "bg-red"
+          isValid || somethingWrong ? "" : "bg-red dark:bg-neon-red-light"
         }`}
       >
         <BackButton href="/" />
         <form onSubmit={submitForm}>
           <div className="flex flex-col gap-6 justify-center items-center">
             <label
-              className="text-3xl lg:text-5xl transition-all ease-in-out"
+              className="text-2xl lg:text-5xl text-center transition-all ease-in-out"
               htmlFor="topicInput"
             >
               {somethingWrong
@@ -60,7 +60,7 @@ export default function AiPage() {
                 ? "Enter A Topic"
                 : "Topic cannot be blank!"}
             </label>
-            <div className="flex flex-row gap-4">
+            <div className="flex flex-col xs:flex-row md:flex-row lg:flex-row gap-4">
               <input
                 autoFocus
                 id="topicInput"
@@ -69,21 +69,22 @@ export default function AiPage() {
                 onChange={onInputChange}
                 placeholder="Enter A Topic"
                 maxLength={50}
-                className="text-gray h-8 text-1xl lg:text-3xl lg:h-16 px-4 rounded-full flex-grow"
+                className="text-gray dark:text-neon-white dark:bg-neon-gray focus:dark:outline-neon-green transition-colors ease-in-out h-12 text-1xl lg:text-3xl lg:h-16 px-4 rounded-full flex-grow"
               />
               <select
-                name="diffculty"
+                name="difficulty"
                 id="difficulty"
                 value={difficulty}
                 onChange={(e) => setDifficulty(Number(e.target.value))}
                 className="form-select appearance-none
-              h-8
+              h-12
               px-4
               lg:h-16
               text-center
               text-1xl
               lg:text-3xl
               bg-white text-black bg-clip-padding bg-no-repeat
+              dark:bg-neon-black dark:text-neon-white
               border-4 border-white
               rounded-full
               transition
@@ -91,6 +92,9 @@ export default function AiPage() {
               hover:bg-black
               hover:text-white
               hover:border-gray
+              hover:dark:bg-neon-gray
+              hover:dark:text-neon-white
+              hover:dark:border-neon-red
               hover:cursor-pointer
               focus:outline-none"
               >
