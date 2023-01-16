989ea2d37ec3cef044b1f9f6efb3b818d0f3125b
Li Yuxuan <44829092+xmliszt@users.noreply.github.com>
2023-01-16 19:15:13 +0800

    Feature: add Open Graph meta tag

--- app/head.tsx
+++ app/head.tsx
@@ -4,6 +4,9 @@ export default function Head() {
       <title>Taboo.AI</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="theme-color" content="#4c453f" />
+      <meta property="og:title" content="Taboo.AI: Play with AI" />
+      <meta property="og:description" content="Trick AI into saying the TABOO words without you saying out the words. Powered by OpenAI API." />
+      <meta property="og:image" content="https://i.ibb.co/q0dBwjW/D0-B19-DF6-0-DB5-43-AB-B323-7-C429-AC72736.jpg" />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
