933a922c4b856b228f1b397e4ae43101ea245b25
Li Yuxuan <xmliszt@gmail.com>
2023-01-17 10:46:03 +0800

    Feature: improve screenshot quality

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -63,7 +63,7 @@ export default function ResultPage() {
   const share = () => {
     if (screenshotRef.current && level) {
       html2canvas(screenshotRef.current, {
-        scale: 2,
+        scale: 3.3,
         allowTaint: true,
         backgroundColor: "#4c453e",
       }).then((canvas) => {
