2b1f394cf78a4b4a0cfb7ff771cc10ece5c5b26f
Li Yuxuan <44829092+xmliszt@users.noreply.github.com>
2023-01-22 22:22:19 +0800

    Feature: HTML meta, sitemap, rule page UI fix

--- app/head.tsx
+++ app/head.tsx
@@ -1,18 +1,27 @@
 export default function Head() {
   return (
     <>
-      <title>Taboo.AI</title>
+      <title>Taboo.AI: Play Word Tricks with AI</title>
       <meta content='width=device-width, initial-scale=1' name='viewport' />
+      <meta charSet='UTF-8' />
       <meta name='theme-color' content='#4c453f' />
+      <meta
+        name='description'
+        content='Play word tricks with ChatGPT AI. Trick the AI into saying the Taboo Words. Ask AI to generate the Taboo Words by topics. Powered by OpenAI.'
+      />
       <meta property='og:title' content='Taboo.AI: Trick AI with Words!' />
       <meta
         property='og:description'
-        content='Trick AI into saying the TABOO words without you saying out the words. Light & Dark Mode Supported! Powered by Next.JS & OpenAI API.'
+        content='Play word tricks with ChatGPT AI. Trick the AI into saying the Taboo Words. Ask AI to generate the Taboo Words by topics. Powered by OpenAI.'
       />
       <meta
         property='og:image'
         content='https://i.ibb.co/8xFQX3P/TabooAI.png'
       />
+      <meta property='og:url' content='https://taboo-ai.vercel.app/' />
+      <meta property='og:site_name' content='Taboo.AI' />
+      <meta property='og:locale' content='en_US' />
+      <meta property='og:type' content='website' />
       <link rel='icon' href='/favicon.ico' />
     </>
   );
--- app/rule/page.tsx
+++ app/rule/page.tsx
@@ -2,7 +2,7 @@ import BackButton from '../(components)/BackButton';
 
 export default function RulePage() {
   return (
-    <>
+    <section className='w-full h-full overflow-hidden'>
       <BackButton />
       <h1
         data-testid='heading-rule-title'
@@ -12,35 +12,38 @@ export default function RulePage() {
       </h1>
       <section
         data-testid='level-section'
-        className='w-full h-full pt-20 pb-16 px-12 flex justify-center items-center overflow-hidden drop-shadow-lg'
+        className='w-full h-full pt-20 pb-16 px-12 flex justify-center items-center drop-shadow-lg'
       >
-        <article className='w-full h-full lg:w-[80%] bg-gray dark:bg-neon-gray dark:text-neon-white text-white p-6 rounded-xl drop-shadow-xl overflow-y-scroll text-md lg:text-2xl'>
+        <article className='w-full h-full lg:w-[80%] bg-gray dark:bg-neon-gray dark:text-neon-white text-white p-6 rounded-xl drop-shadow-xl text-md lg:text-2xl overflow-y-scroll'>
           <p className='text-center dark:text-neon-white'>
-            Taboo.AI has only one simple rule
+            Taboo.AI is a game of <b>Taboo</b> with a twist. In this game, you
+            are playing against AI!
           </p>
           <br />
           <h3 className='text-center text-lg lg:text-3xl font-bold text-yellow dark:text-neon-green'>
-            Talk to the AI and try to trick it into speaking out the
-            &apos;Taboo&apos; word shown on top of the screen.
+            <b>Guess Words</b> will be given to you. Your job is to converse
+            with the AI and trick it into saying the &apos;Guess Words&apos;.
           </h3>
 
           <br />
           <p className='text-justify'>
-            1. Select a category of words, or choose the <b>AI MODE</b> to give
-            AI a topic and let AI generates the <b>TABOO</b> words for you!{' '}
+            1. Select a category of words, or choose the <b>AI MODE</b> to let
+            AI generate the <b>Guess Words</b> based on custom topics!{' '}
           </p>
           <br />
           <p className='text-justify'>
             2. Start your conversation with the AI and try to trick it into
-            speaking out the <b>TABOO</b> word.
+            speaking out the <b>Guess Words</b>. Watch the <b>Timer</b>, the
+            longer you spend talking to the AI, the lower your score will be
+            eventually!
           </p>
           <br />
           <p className='text-justify'>
-            3. Complete all 5 words to see your scores and share with your
-            friends!
+            3. Successfully trick AI into saying <b>all 5 words</b> given to win
+            the game. You can view your scores and share them with your friends!
           </p>
         </article>
       </section>
-    </>
+    </section>
   );
 }
