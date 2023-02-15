4a54417f75e9693e3307435da4c168a3a9ad5e71
Li Yuxuan <44829092+xmliszt@users.noreply.github.com>
2023-02-16 00:13:28 +0800

    [SEO] Improve SEO keywords

--- app/head.tsx
+++ app/head.tsx
@@ -1,44 +1,56 @@
 export default function Head() {
   return (
     <>
-      <title>Taboo.AI: Play Word Tricks with AI</title>
+      <title>Taboo.AI: Play Taboo Game with AI</title>
       <meta content='width=device-width, initial-scale=1' name='viewport' />
       <meta charSet='UTF-8' />
       <meta
         name='description'
-        content='Play word tricks with ChatGPT AI. Trick the AI into saying the Taboo Words. Ask AI to generate the Taboo Words by topics. Powered by OpenAI.'
+        content='Play Taboo Game for free with ChatGPT AI. Trick the AI into saying the Taboo Words. Ask AI to generate the Taboo Words by topics. Powered by OpenAI.'
       />
-      <meta property='og:title' content='Taboo.AI: Trick AI with Words!' />
+      <meta property='og:title' content='Taboo.AI: Play Taboo Game with AI' />
       <meta
         property='og:description'
-        content='Play word tricks with ChatGPT AI. Trick the AI into saying the Taboo Words. Ask AI to generate the Taboo Words by topics. Powered by OpenAI.'
+        content='Play Taboo Game for free with ChatGPT AI. Trick the AI into saying the Taboo Words. Ask AI to generate the Taboo Words by topics. Powered by OpenAI.'
       />
       <meta
         property='og:image'
         content='https://i.ibb.co/8xFQX3P/TabooAI.png'
       />
-      <meta property='og:image:alt' content='Taboo.AI: Trick AI with Words' />
+      <meta
+        property='og:image:alt'
+        content='Taboo.AI: Play Taboo Game for Free with AI'
+      />
       <meta property='og:url' content='https://taboo-ai.vercel.app/' />
       <meta property='og:site_name' content='Taboo.AI' />
       <meta property='og:locale' content='en_US' />
       <meta property='og:type' content='website' />
       <meta
         name='keywords'
-        content='taboo,ai,taboo ai,chatgpt,openai,word game,game,nextjs,vercel,web app,web game,charades,online game,topics,knowledge,guess,conversation,score,rank'
+        content='taboo,online,free,game,ai,taboo ai,chatgpt,openai,word game,nextjs,vercel,web app,web game,charades,online game,topics,knowledge,guess,conversation,score,rank'
       />
       <meta name='author' content='Li Yuxuan' />
-      <meta name='application-name' content='Taboo.AI: Trick AI with Words' />
+      <meta
+        name='application-name'
+        content='Taboo.AI: Play Taboo Game for Free with AI'
+      />
       <meta name='twitter:card' content='summary_large_image' />
-      <meta name='twitter:title' content='Taboo.AI: Trick AI with Words!' />
+      <meta
+        name='twitter:title'
+        content='Taboo.AI: Play Taboo Game for Free with AI'
+      />
       <meta
         name='twitter:description'
-        content='Play word tricks with ChatGPT AI. Trick the AI into saying the Taboo Words. Ask AI to generate the Taboo Words by topics. Powered by OpenAI.'
+        content='Play Taboo Game for free ChatGPT AI. Trick the AI into saying the Taboo Words. Ask AI to generate the Taboo Words by topics. Powered by OpenAI.'
       />
       <meta
         name='twitter:image'
         content='https://i.ibb.co/8xFQX3P/TabooAI.png'
       />
-      <meta name='twitter:image:alt' content='Taboo.AI: Trick AI with Words' />
+      <meta
+        name='twitter:image:alt'
+        content='Taboo.AI: Play Taboo Game for Free with AI'
+      />
       <meta name='twitter:creator' content='@xmliszt' />
       <meta name='twitter:site' content='@xmliszt' />
       <meta
