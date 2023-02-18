d23a1696057bc10b7a4b0fc20f7cfd16b512ee26
Li Yuxuan <44829092+xmliszt@users.noreply.github.com>
2023-02-18 19:33:40 +0800

    Bugfix: overflow no wrap for badges

--- app/levels/(components)/Badge.tsx
+++ app/levels/(components)/Badge.tsx
@@ -12,7 +12,7 @@ export default function Badge(props: BadgeProps) {
       <span
         className={`absolute drop-shadow-lg -top-2 lg:-top-4 -right-8 h-4 lg:h-8 text-xs lg:text-xl w-auto px-2 lg:px-4 z-10 text-white dark:text-neon-black rounded-full font-mono ${
           props.customClass ?? 'bg-black dark:bg-neon-black'
-        }`}
+        } whitespace-nowrap`}
       >
         {props.label}
       </span>
