bf128223c27f7ca2cb8cd1245f9cb0fbd5212ca5
Li Yuxuan <44829092+xmliszt@users.noreply.github.com>
2023-02-23 00:27:59 +0800

    Bugfix: light dark switch layout no change

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,10 +6,13 @@ import { Orbitron, Grenze } from '@next/font/google';
 import { AnalyticsWrapper } from './(components)/AnalayticsWrapper';
 import WordCarousell from './(components)/WordCarousell';
 import { usePathname } from 'next/navigation';
-import LightDarkSwitchButton, {
-  Theme,
-} from './(components)/LightDarkSwitchButton';
 import { useState, useEffect } from 'react';
+import { MdDarkMode, MdOutlineWbTwilight } from 'react-icons/md';
+
+export enum Theme {
+  Light = 'light',
+  Dark = 'dark',
+}
 
 const grenze = Grenze({
   weight: '400',
@@ -54,8 +57,30 @@ export default function RootLayout({
     isMounted && setIsDark(localStorage.getItem('theme') === 'dark');
   }, [isMounted]);
 
-  const onThemeChange = (theme: Theme) => {
-    setIsDark(theme === Theme.Dark);
+  const onToggle = () => {
+    const dark = !isDark;
+    setIsDark(dark);
+    const theme = dark ? 'dark' : 'light';
+    localStorage.setItem('theme', theme);
+    setIsDark(theme === 'dark');
+  };
+
+  const getLocationClass = () => {
+    switch (pathName) {
+      case '/':
+        return 'top-5 left-5';
+      case '/result':
+      case '/level':
+      case '/whatsnew':
+      case '/buymecoffee':
+        return 'top-4 lg:top-3.5 left-12 lg:left-20';
+      case '/levels':
+      case '/ai':
+      case '/rule':
+        return 'top-4 right-5 lg:top-3.5';
+      default:
+        return 'bottom-5 left-5';
+    }
   };
 
   return (
@@ -68,10 +93,16 @@ export default function RootLayout({
       <head />
       <body className='bg-black dark:bg-neon-black dark:text-neon-white text-white'>
         {!(pathName?.match(/^\/level$/)?.length ?? 0 > 0) && <WordCarousell />}
-        <LightDarkSwitchButton
-          onThemeChanged={onThemeChange}
-          pathName={pathName}
-        />
+        <button
+          id='theme'
+          aria-label='toggle light/dark button'
+          data-testid='light-dark-toggle-button'
+          className={`fixed z-50 ${getLocationClass()} opacity-100 hover:animate-pulse transition-all text-2xl lg:text-5xl dark:text-neon-blue
+      }`}
+          onClick={onToggle}
+        >
+          {isDark ? <MdDarkMode /> : <MdOutlineWbTwilight />}
+        </button>
         {children}
         <AnalyticsWrapper />
       </body>
