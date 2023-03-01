06ac5bb6f5ffe58637058d6ba5949d205f85c428
Li Yuxuan <44829092+xmliszt@users.noreply.github.com>
2023-03-01 12:29:28 +0800

    Hotfix: outage data backend parse error

--- pages/api/maintenance.tsx
+++ pages/api/maintenance.tsx
@@ -1,13 +1,22 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
+interface IOutageData {
+  isGPTOutage: boolean;
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const isGPTOutage = Boolean(process.env.CHATGPT_OUTAGE);
+  const outageData: IOutageData = {
+    isGPTOutage: false,
+  };
+  if (process.env.CHATGPT_OUTAGE) {
+    outageData.isGPTOutage = JSON.parse(process.env.CHATGPT_OUTAGE);
+  }
   if (req.method === 'GET') {
     try {
-      res.json({ isGPTOutage });
+      res.json(outageData);
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
