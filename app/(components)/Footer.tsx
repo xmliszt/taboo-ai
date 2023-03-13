2a5106f476761ea517331bf8baa6af3a73cba8ee
Li Yuxuan <44829092+xmliszt@users.noreply.github.com>
2023-03-13 22:18:40 +0800

    Feature: add policies

--- app/(components)/Footer.tsx
+++ app/(components)/Footer.tsx
@@ -1,14 +1,22 @@
+'use client';
+
 import { TbBrandNextjs } from 'react-icons/tb';
 import { SiOpenai } from 'react-icons/si';
 import { FiGithub } from 'react-icons/fi';
+
+declare global {
+  interface Window {
+    displayPreferenceModal: () => void;
+  }
+}
 
 export default function Footer() {
   return (
     <footer
       data-testid='footer'
-      className='w-full h-12 rounded-t-2xl dark:drop-shadow-[0_-10px_30px_rgba(0,0,0,1)] flex justify-center items-center bg-gray dark:bg-neon-gray fixed bottom-0 z-50 text-white dark:text-neon-white lg:text-white-faded lg:dark:text-neon-white text-center text-xs lg:text-lg'
+      className='w-full h-12 rounded-t-2xl dark:drop-shadow-[0_-10px_30px_rgba(0,0,0,1)] flex flex-col gap-0 justify-center items-center bg-gray dark:bg-neon-gray fixed bottom-0 z-50 text-white dark:text-neon-white lg:text-white-faded lg:dark:text-neon-white text-center text-xs lg:text-lg'
     >
-      <article className='flex-grow px-2'>
+      <article className='px-2'>
         Powered by{' '}
         <a
           aria-label='Read more about Next.JS on its official website'
@@ -35,6 +43,35 @@ export default function Footer() {
           className='underline text-white dark:text-neon-blue lg:hover:text-white lg:hover:dark:text-neon-red transition-colors'
         >
           Li Yuxuan <FiGithub className='inline' />
+        </a>
+      </article>
+      <article className='px-2 flex flex-row gap-2'>
+        <a
+          aria-label='Read more about the Privacy Policy'
+          href='/privacy'
+          className='underline text-white dark:text-neon-green lg:hover:text-white lg:hover:dark:text-neon-white transition-colors'
+        >
+          Privacy Policy
+        </a>
+        <a
+          aria-label='Read more about the Cookie Policy'
+          href='/cookie-policy'
+          className='underline text-white dark:text-neon-green lg:hover:text-white lg:hover:dark:text-neon-white transition-colors'
+        >
+          Cookie Policy
+        </a>
+        <a
+          aria-label='Open Preference Center to manage cookies'
+          href='#'
+          onClick={(e) => {
+            e.preventDefault();
+            window.displayPreferenceModal();
+            return false;
+          }}
+          id='termly-consent-preferences'
+          className='underline text-white dark:text-neon-blue lg:hover:text-white lg:hover:dark:text-neon-red transition-colors'
+        >
+          Consent Preferences
         </a>
       </article>
     </footer>
