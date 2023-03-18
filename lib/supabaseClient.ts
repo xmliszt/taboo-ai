f4986715e91fa431b504ffdd441d87acbbb66b5a
Li Yuxuan <xmliszt@gmail.com>
2023-03-18 20:30:48 +0800

    Preview (#47)
    * Remove dependency on game related DB operations
    
    * Fix: development workflow
    
    * Remove dev workflow as it runs locally, change preview workflow for preview branch
    
    * Fix: preview workflow

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
+console.log(process.env);
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
   process.env.NEXT_PUBLIC_ANON_KEY ?? ''
