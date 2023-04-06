4adce44c1f5d5dbad36826a9831a549d677c63f6
Li Yuxuan <44829092+xmliszt@users.noreply.github.com>
2023-04-06 10:37:42 +0800

    Bugfix: no player table exist

--- supabase/migrations/20230406015644_add_ai_score_and_explanation_to_score.sql
+++ supabase/migrations/20230406015644_add_ai_score_and_explanation_to_score.sql
@@ -1,11 +1,5 @@
 alter table "public"."game" drop constraint "fk_daily_level_name";
 
-alter table "public"."player" drop constraint "player_pkey";
-
-drop index if exists "public"."player_pkey";
-
-drop table "public"."player";
-
 alter table "public"."score" add column "ai_explanation" text;
 
 alter table "public"."score" add column "ai_score" integer;
