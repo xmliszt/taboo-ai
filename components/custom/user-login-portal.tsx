676493fbf7e71ea5386c4135ceaf5304942acf36
Li Yuxuan <44829092+xmliszt@users.noreply.github.com>
2023-12-11 23:25:34 +0800

    feat: allow user to manage subscription

--- components/custom/user-login-portal.tsx
+++ components/custom/user-login-portal.tsx
@@ -33,13 +33,14 @@ import { Badge } from '../ui/badge';
 import { Button } from '../ui/button';
 import { CustomEventKey, EventManager } from '@/lib/event-manager';
 import { cn } from '@/lib/utils';
+import { createCustomerPortalSession } from '@/lib/services/subscriptionService';
 
 interface UserMenuItem {
   label: string;
   icon: React.ReactElement;
   isVisible: boolean;
   isUpcoming?: boolean;
-  onClick: (event: Event) => void;
+  onClick: ((event: Event) => void) | ((event: Event) => Promise<void>);
 }
 
 export function UserLoginPortal() {
@@ -69,15 +70,31 @@ export function UserLoginPortal() {
     }
   };
 
+  const handleManageSubscription = async () => {
+    if (!user?.customerId) return;
+    try {
+      const portalSessionUrl = await createCustomerPortalSession(
+        user.customerId,
+        `${window.location.origin}/profile?anchor=subscription`
+      );
+      router.push(portalSessionUrl);
+    } catch (error) {
+      console.error(error);
+      toast({
+        title:
+          'Sorry, we are unable to manage your subscription. Please try again!',
+        variant: 'destructive',
+      });
+    }
+  };
+
   const userMenuItems: UserMenuItem[] = useMemo(() => {
     return [
       {
-        label: 'Change Subscription',
+        label: 'Manage Subscription',
         icon: <BookMarked />,
-        isVisible: pathname !== '/pricing',
-        onClick: () => {
-          router.push('/pricing');
-        },
+        isVisible: user?.customerId !== undefined,
+        onClick: handleManageSubscription,
       },
       {
         label: 'Contribute A Topic',
@@ -110,7 +127,7 @@ export function UserLoginPortal() {
         onClick: handleLogout,
       },
     ];
-  }, [pathname, game, status]);
+  }, [pathname, game, status, user?.customerId]);
 
   const handleLogin = async () => {
     if (!login) return;
