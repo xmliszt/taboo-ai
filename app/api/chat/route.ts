14b95555726092865267c304bf1a90b0789f153a
Li Yuxuan <xmliszt@gmail.com>
2023-12-18 09:43:39 +0800

    fix typo

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-import { tryParseErrorAsGoogleAIError } from '@/lib/errors./google-ai-error-parser';
+import { tryParseErrorAsGoogleAIError } from '@/lib/errors/google-ai-error-parser';
 import { googleGeminiPro } from '@/lib/google-ai';
 import { IChat } from '@/lib/types/score.type';
 
