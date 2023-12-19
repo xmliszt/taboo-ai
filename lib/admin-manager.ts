5b664b78317212f7ae8e5dd44ec1c16e457afc9c
Li Yuxuan <44829092+xmliszt@users.noreply.github.com>
2023-12-20 00:20:55 +0800

    Update whitelistedUIDs in AdminManager for preview

--- lib/admin-manager.ts
+++ lib/admin-manager.ts
@@ -3,7 +3,7 @@ import IUser from './types/user.type';
 export class AdminManager {
   private static whitelistedUIDs = [
     'BnlcfMNIvrf2XCxY73O5KXmYNkI3', // production
-    'kWNBkcDCPjWGzsB9ez4UP7Hqv9w2', // preview
+    'XfOSvJfXAtTX46uRETWTGdBRGlI2', // preview
   ];
 
   static checkIsAdmin(user: IUser | null | undefined): boolean {
