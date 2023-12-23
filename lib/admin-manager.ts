edef69f812b1d8bf5b29e3f7c95552d8070dcbe1
Li Yuxuan <44829092+xmliszt@users.noreply.github.com>
2023-12-23 17:49:43 +0800

    Whitelist Austin for dev access

--- lib/admin-manager.ts
+++ lib/admin-manager.ts
@@ -2,12 +2,13 @@ import IUser from './types/user.type';
 
 export class AdminManager {
   static WHITELIST_UIDS = [
-    'BnlcfMNIvrf2XCxY73O5KXmYNkI3', // production
-    'aynNbQcI5rV2UxXVaRRW34K3eaH2', // preview
+    'BnlcfMNIvrf2XCxY73O5KXmYNkI3', // production - Yuxuan
+    'aynNbQcI5rV2UxXVaRRW34K3eaH2', // preview - Yuxuan
+    'xAlLvLRvY7NHtRr5GkL6TxIV0Vu1', // production - Austin
+    'd57nt5xyZhX4S4f1sOFlo5WbAlX2', // preview - Austin
   ];
 
   static checkIsAdmin(user: IUser | null | undefined): boolean {
-    console.log(user);
     if (user?.uid) {
       return this.WHITELIST_UIDS.includes(user.uid);
     }
