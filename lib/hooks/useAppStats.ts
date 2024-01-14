043082d36a2ce27da38a80a6c586be9db55c3178
Li Yuxuan <xmliszt@gmail.com>
2024-01-15 03:11:09 +0800

    fix build issue with firebase import in appstats

--- lib/hooks/useAppStats.ts
+++ lib/hooks/useAppStats.ts
@@ -1,17 +1,19 @@
 import { useCallback, useEffect, useState } from 'react';
-import { DataSnapshot } from 'firebase/database';
 
 import { listenToAppStats } from '../services/appService';
 
 export interface AppStatsProps {
   views: number;
 }
+
+// TODO: Update hook to use supabase realtime
 export const useAppStats = () => {
   const [stats, setStats] = useState<AppStatsProps>();
 
-  const onSnapshotUpdated = useCallback((snapshot: DataSnapshot) => {
-    const data = snapshot.val() as AppStatsProps;
-    setStats(data);
+  const onSnapshotUpdated = useCallback(() => {
+    setStats({
+      views: 0,
+    });
   }, []);
 
   useEffect(() => {
