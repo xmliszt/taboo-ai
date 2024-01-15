56010085cea835fff3054347e1614c64fcd4ebd4
Li Yuxuan <xmliszt@gmail.com>
2024-01-15 23:08:39 +0800

    integrate realtime app stats for views

--- lib/hooks/useAppStats.ts
+++ lib/hooks/useAppStats.ts
@@ -1,24 +1,54 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { AsyncReturnType } from 'type-fest';
 
-import { listenToAppStats } from '../services/appService';
+import { createClient } from '@/lib/utils/supabase/client';
 
-export interface AppStatsProps {
-  views: number;
-}
-
-// TODO: Update hook to use supabase realtime
 export const useAppStats = () => {
-  const [stats, setStats] = useState<AppStatsProps>();
-
-  const onSnapshotUpdated = useCallback(() => {
-    setStats({
-      views: 0,
-    });
-  }, []);
+  const [appStats, setAppStats] = useState<{ [key: string]: AppStats }>({});
 
   useEffect(() => {
-    listenToAppStats();
-  }, [onSnapshotUpdated]);
-
-  return { stats };
+    const supabaseClient = createClient();
+    // Fetch app_stats for initial render
+    async function fetchInitialAppStats() {
+      try {
+        const appStats = await fetchAppStats();
+        const appStatsObject: { [key: string]: AppStats } = {};
+        for (const appStat of appStats) {
+          appStatsObject[appStat.id] = appStat;
+        }
+        setAppStats(appStatsObject);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    void fetchInitialAppStats();
+    // Subscribe to app_stats changes
+    supabaseClient
+      .channel('app_stats')
+      .on(
+        'postgres_changes',
+        {
+          event: 'UPDATE',
+          schema: 'public',
+          table: 'app_stats',
+        },
+        (payload) => {
+          const appStatsCopy = { ...appStats };
+          const newAppStat = payload.new as AppStats;
+          appStatsCopy[newAppStat.id] = newAppStat;
+          setAppStats(appStatsCopy);
+        }
+      )
+      .subscribe();
+  }, [setAppStats]);
+  return appStats;
 };
+
+async function fetchAppStats() {
+  const supabaseClient = createClient();
+  const fetchAppStatsResponse = await supabaseClient.from('app_stats').select();
+  if (fetchAppStatsResponse.error) throw fetchAppStatsResponse.error;
+  return fetchAppStatsResponse.data;
+}
+
+type AppStats = AsyncReturnType<typeof fetchAppStats>[number];
--- lib/services/appService.ts
+++
@@ -1,7 +0,0 @@
-export const listenToAppStats = () => {
-  // TODO: listen to supabase realtime app stats
-};
-
-export const incrementView = () => {
-  // TODO: increment view count
-};
