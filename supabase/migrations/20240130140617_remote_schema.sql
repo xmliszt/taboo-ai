188f5434f70a310d073ffab66aa72d0bb9c717e4
Li Yuxuan <xmliszt@gmail.com>
2024-01-30 22:37:05 +0800

    fix: update migration to drop policies if exists

--- supabase/migrations/20240130140617_remote_schema.sql
+++ supabase/migrations/20240130140617_remote_schema.sql
@@ -1,7 +1,7 @@
-drop policy "update access to all user" on "public"."app_stats";
+drop policy if exists "update access to all user" on "public"."app_stats";
 
-drop policy "full access to xmliszt@gmail.com" on "public"."levels";
+drop policy if exists "full access to xmliszt@gmail.com" on "public"."levels";
 
-drop policy "full access for xmliszt@gmail.com" on "public"."words";
+drop policy if exists "full access for xmliszt@gmail.com" on "public"."words";
 
 
