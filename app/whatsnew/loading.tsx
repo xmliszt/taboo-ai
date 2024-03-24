40391e890b0addbfb29abcf0a411b698f1ed9fff
Li Yuxuan <xmliszt@gmail.com>
2024-03-24 11:33:08 +0800

    New loading animation (#320)
    * Update loading UI
    
    * Update new feature release.

--- app/whatsnew/loading.tsx
+++ app/whatsnew/loading.tsx
@@ -1,9 +1,9 @@
-import { Skeleton } from '@/components/custom/skeleton';
+import { TabooAILoadingEffect } from '@/components/custom/globals/taboo-ai-loading-effect';
 
 export default function Loading() {
   return (
-    <section className='flex h-full justify-center p-4'>
-      <Skeleton numberOfRows={10} />
+    <section className='flex h-full items-center justify-center p-4'>
+      <TabooAILoadingEffect />
     </section>
   );
 }
