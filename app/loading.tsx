6298f43180c70539c1eb0189f24a2f76d381778f
Li Yuxuan <xmliszt@gmail.com>
2024-03-24 11:13:11 +0800

    Update new feature release.

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,8 +4,8 @@ import { TabooAILoadingEffect } from '@/components/custom/globals/taboo-ai-loadi
 
 export default function Loading() {
   return (
-    <div className='fixed left-0 top-0 z-[1000] flex h-screen w-screen animate-fade-in items-center justify-center backdrop-blur-lg backdrop-brightness-[15%]'>
-      <TabooAILoadingEffect className='[&>#taboo-ai-logo-svg-stroke_path]:!stroke-white' />
+    <div className='fixed left-0 top-0 z-[1000] flex h-screen w-screen animate-fade-in items-center justify-center backdrop-blur-lg backdrop-brightness-100 dark:backdrop-brightness-[15%]'>
+      <TabooAILoadingEffect />
     </div>
   );
 }
