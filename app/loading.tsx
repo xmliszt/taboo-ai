08f68d77f1552b718b422a0f4dca05becee8103d
Li Yuxuan <xmliszt@gmail.com>
2024-03-24 10:52:13 +0800

    Update loading UI

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,11 @@
-import LoadingMask from '../components/custom/loading-mask';
+'use client';
+
+import { TabooAILoadingEffect } from '@/components/custom/globals/taboo-ai-loading-effect';
 
 export default function Loading() {
-  return <LoadingMask isLoading={true} message={'Taboo AI is thinking >_< ...'} />;
+  return (
+    <div className='fixed left-0 top-0 z-[1000] flex h-screen w-screen animate-fade-in items-center justify-center backdrop-blur-lg backdrop-brightness-[15%]'>
+      <TabooAILoadingEffect className='[&>#taboo-ai-logo-svg-stroke_path]:!stroke-white' />
+    </div>
+  );
 }
