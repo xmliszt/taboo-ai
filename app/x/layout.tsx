89b983eafe0221b60f3a24ede12c08ac180d78a5
Li Yuxuan <xmliszt@gmail.com>
2024-04-28 15:59:45 +0800

    fix build issue

--- app/x/layout.tsx
+++ app/x/layout.tsx
@@ -5,6 +5,6 @@ import { fetchUserProfile } from '@/app/profile/server/fetch-user-profile';
 
 export default async function DevLayout({ children }: { children: React.ReactNode }) {
   const user = await fetchUserProfile();
-  if (!user.is_dev) notFound();
+  if (!user?.is_dev) notFound();
   return <> {children} </>;
 }
