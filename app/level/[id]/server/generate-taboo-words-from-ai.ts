4f32939453271b9a2569a098fa965106dd8391cc
Li Yuxuan <xmliszt@gmail.com>
2024-09-11 06:13:30 +0800

    Protect AI server actions (#343)
    * Protect AI server actions
    
    * Sync remote migrations

--- app/level/[id]/server/generate-taboo-words-from-ai.ts
+++ app/level/[id]/server/generate-taboo-words-from-ai.ts
@@ -6,23 +6,26 @@ import { cookies } from 'next/headers';
 import { isString, toLower, trim } from 'lodash';
 import OpenAI from 'openai';
 
+import { fetchUserProfile } from '@/app/profile/server/fetch-user-profile';
+
 const openai = new OpenAI();
 
 /**
  * Ask AI to generate taboo words for a given topic.
  */
 export async function generateTabooWordsFromAI(targetWord: string, topic?: string) {
+  // Permission: check that only logged-in user can call this.
+  const user = await fetchUserProfile();
+  if (!user) throw new Error('Unauthorized');
+
   cookies(); // opt-out for caching
   console.log(`Generating taboo words for ${targetWord} in ${topic}. Topic is omitted currently.`);
   const target = toLower(trim(targetWord));
   const systemPrompt = `
-  You are tasked to generate 5-8 words related to a given target word in a given topic by user, in American English. Avoid plural and duplicates. You only respond in JSON format. Output your response in the following format:
+  You are tasked to generate 8 words related to a given target word given by user, in American English. Avoid plural and duplicates. You only respond in JSON. Output your response in the following format:
   { "words": ["word1", "word2", ...] }
   `;
-  const userPrompt = JSON.stringify({
-    target,
-    topic,
-  });
+  const userPrompt = JSON.stringify({ target });
 
   const completion = await openai.chat.completions.create({
     model: 'gpt-3.5-turbo',
