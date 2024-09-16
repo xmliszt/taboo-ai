b88b4a85a247420214630da93feb0be10ca6435f
Li Yuxuan <xmliszt@gmail.com>
2024-09-16 09:12:42 +0800

    Remove levels from sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,22 +1,14 @@
 import { MetadataRoute } from 'next';
 
-import { fetchAllLevels } from '@/app/levels/server/fetch-levels';
 import { RouteManager } from '@/lib/utils/routeUtils';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const staticRoutes = RouteManager.getStaticRoutes();
-  const levels = await fetchAllLevels();
   const staticSites = staticRoutes.map((route) => {
     return {
       url: route,
       lastModified: new Date(),
     };
   });
-  const levelSites = levels.map((level) => {
-    return {
-      url: `${RouteManager.baseUrl}/level/${level.id}`,
-      lastModified: new Date(),
-    };
-  });
-  return [...staticSites, ...levelSites];
+  return staticSites;
 }
