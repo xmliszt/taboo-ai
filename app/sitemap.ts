4dbfe3fb6c777e915a3643e850d7d7f8df008a2b
Li Yuxuan <xmliszt@gmail.com>
2024-09-16 09:13:57 +0800

    Remove /x route from sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,14 +1,18 @@
 import { MetadataRoute } from 'next';
+import { compact } from 'lodash';
 
 import { RouteManager } from '@/lib/utils/routeUtils';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const staticRoutes = RouteManager.getStaticRoutes();
-  const staticSites = staticRoutes.map((route) => {
-    return {
-      url: route,
-      lastModified: new Date(),
-    };
-  });
+  const staticSites = compact(
+    staticRoutes.map((route) => {
+      if (route.includes('/x/review-words')) return;
+      return {
+        url: route,
+        lastModified: new Date(),
+      };
+    })
+  );
   return staticSites;
 }
